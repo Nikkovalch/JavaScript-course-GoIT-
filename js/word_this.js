f06570161ab24dda9dfa9f6708666217b0568b2c
Mykola Kovalchuk <nikkovalchwork@gmail.com>
2024-03-03 21:05:45 +0200

    completed the task 4

---
+++ js/word_this.js
@@ -0,0 +1,20 @@
+// const customer = {
+//   firstName: "Jacob",
+//   lastName: "Mercer",
+//   getFullName() {
+//     return `${this.firstName} ${this.lastName}`;
+//   },
+// };
+
+// function makeMessage(callback) {
+//   // callback() - це виклик методу getFullName без об'єкта
+//   console.log(`Обробляємо заявку від ${callback()}.`);
+// }
+
+// makeMessage(customer.getFullName);
+
+// const showThis = () => {
+//   console.log("this in showThis: ", this);
+// };
+
+// showThis();
