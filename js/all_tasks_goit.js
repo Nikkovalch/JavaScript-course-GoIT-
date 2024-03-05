c2af5bba59ef2444eba4687bcb85ad2a48cf817c
Mykola Kovalchuk <nikkovalchwork@gmail.com>
2024-03-05 22:34:21 +0200

    created task

--- js/all_tasks_goit.js
+++ js/all_tasks_goit.js
@@ -855,3 +855,122 @@
 // }
 
 // console.log(calcTotalPrice(stones, 'Сапфір'));
+
+
+// Example 4 - Комплексні завдання
+
+// Напиши скрипт управління особистим кабінетом інтернет банку. 
+// Є об'єкт account в якому необхідно реалізувати методи для роботи з балансом та історією транзакцій.
+
+/*
+ * Типів транзакцій всього два.
+ * Можна покласти чи зняти гроші з рахунку.
+ */
+const Transaction = {
+  DEPOSIT: 'deposit',
+  WITHDRAW: 'withdraw',
+};
+
+/*
+ * Кожна транзакція це об'єкт із властивостями: id, type та amount
+ */
+
+const account = {
+  // Поточний баланс рахунку
+  balance: 0,
+  // id
+  // id: Number((Math.random() * 2).toString().slice(2,6)),
+  // Історія транзакцій
+  transactions: [],
+
+
+  /*
+   * Метод створює та повертає об'єкт транзакції.
+   * Приймає суму та тип транзакції.
+   */
+  createTransaction(amount, type) {
+    this.transactions.push({[type]: amount, id: Math.floor(Math.random() * 1000000)})
+  },
+  
+  /*
+   * Метод, що відповідає за додавання суми до балансу.
+   * Приймає суму транзакції.
+   * Викликає createTransaction для створення об'єкта транзакції
+   * після чого додає його до історії транзакцій
+   */
+  deposit(amount) {
+    this.createTransaction(amount, Transaction.DEPOSIT)
+    this.balance += amount
+  },
+
+  /*
+   * Метод, що відповідає за зняття суми з балансу.
+   * Приймає суму транзакції.
+   * Викликає createTransaction для створення об'єкта транзакції
+   * після чого додає його до історії транзакцій.
+   *
+   * Якщо amount більше ніж поточний баланс, виводь повідомлення
+   * про те, що зняття такої суми не можливе, недостатньо коштів.
+   */
+  withdraw(amount) {
+    this.createTransaction(amount, Transaction.WITHDRAW)
+    return amount > this.balance 
+    ? console.log("Зняття такої суми не можливе, недостатньо коштів" )
+    : this.balance -= amount
+  },
+
+  /*
+   * Метод повертає поточний баланс
+   */
+  getBalance() {
+    return this.balance
+  },
+
+  /*
+   * Метод шукає та повертає об'єкт транзакції по id
+   */
+  getTransactionDetails(id) {
+    for(const obj of this.transactions) {
+      if(id === obj.id) {
+        return obj
+      }
+    }
+  },
+
+  /*
+   * Метод повертає кількість коштів
+   * певного типу транзакції з усієї історії транзакцій
+   */
+  getTransactionTotal(type) {
+    let total = 0;
+    let found;
+
+    for(const transaction of this.transactions) {
+      const keys = Object.keys(transaction)
+
+      if(keys[0] === type) {
+        total += transaction[type]
+        found = true
+      } 
+
+    }
+    if(!found) {
+      console.log("Транзакції з таким типом не існує");
+    }
+    return total
+  },
+
+
+};
+//"Транзакції з таким типом не існує"
+
+account.deposit(200)
+account.withdraw(20)
+account.deposit(200)
+
+account.withdraw(20)
+account.withdraw(100)
+account.deposit(200)
+
+console.log(account.transactions);
+console.log(account.getTransactionTotal(Transaction.DEPOST));
