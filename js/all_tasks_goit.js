a3366bdbf3a86c86ecc26efd516553c080353ddd
Mykola Kovalchuk <nikkovalchwork@gmail.com>
2024-03-14 22:12:22 +0200

    Arrow function

--- js/all_tasks_goit.js
+++ js/all_tasks_goit.js
@@ -1177,3 +1177,22 @@
 //     friendCount: 20,
 //   }),
 // );
+
+
+// const calсulateAverage = (...args) => {
+//   let total = 0;
+//   let arrLength;
+//   args.forEach(function (num, idx, arr) {
+//     total += num 
+//     arrLength = arr.length
+//   })
+//   // let total = 0;
+//   // for (let i = 0; i < args.length; i++) {
+//   //   total += args[i];
+//   // }
+//   return total / arrLength
+// }
+
+// console.log(calсulateAverage(1, 2, 3, 4)); // 2.5
+// console.log(calсulateAverage(14, 8, 2)); // 8
+// console.log(calсulateAverage(27, 43, 2, 8, 36)); // 23.2
